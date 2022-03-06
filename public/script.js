aa3c9fb7d85a0da787616627ba6e8bead02903ef
muftauakindele <dayoakindele@gmail.com>
2022-03-05 18:50:00 -0600

    first commit

---
+++ public/script.js
@@ -0,0 +1,49 @@
+//Faders
+const faders = document.querySelectorAll(".fadeIn");
+const sliders = document.querySelectorAll(".slideIn");
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -65px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider);
+});
+const allLinks = document.querySelectorAll("a:link");
+
+/////////////////////////////////////////////////////////
+//Smooth scrolling animation
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
+
+    //Link scroll function
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      console.log(sectionEl);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
